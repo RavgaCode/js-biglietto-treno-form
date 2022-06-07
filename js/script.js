bbebf16765ec6d8704229bc8eada31a38c8dd873
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-06-07 15:07:55 +0200

    Fix variabili dentro alla funzione

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// Dichiaro le mie costanti legate ai valori immessi dall'utente
-const userName = document.getElementById("user-name").value;
-const userKm = parseInt(document.getElementById("user-km").value);
-const userAge = document.getElementById("user-age").value;
 // Imposto l'evento per generare il ticket al click del button
 const generateButton = document.getElementById("generate-ticket");
 generateButton.addEventListener("click", ticketGenerate);
 
 // Funzione per la stampa del biglietto
 function ticketGenerate() {
+  // Dichiaro le mie costanti legate ai valori immessi dall'utente
+  const userName = document.getElementById("user-name").value;
+  const userKm = parseInt(document.getElementById("user-km").value);
+  const userAge = document.getElementById("user-age").value;
   // Calcolo del prezzo del ticket e stampa dei dati
   const price = userKm * 0.21;
   const discountYoung = 20 / 100;
@@ -19,19 +19,27 @@ function ticketGenerate() {
   } else if (userAge === "senior") {
     discount = price * discountSenior;
     document.getElementById("ticket-offer").innerText = "Senior";
-  } else {
+  } else if (userAge === "adult") {
     document.getElementById("ticket-offer").innerText = "Standard";
   }
   const finalPrice = price - discount;
 
+  // Imposto le constanti per la carrozza e il numero di biglietto
   const ticketNumber = Math.floor(Math.random() * 99999);
-  const ticketWagon = Math.floor(Math.random() * 11 + 1);
+  const ticketCarriage = Math.floor(Math.random() * 11 + 1);
 
+  // Stampo tutto a pagina
   document.getElementById("ticket-name").innerText = userName;
   document.getElementById("ticket-price").innerText = `${finalPrice.toFixed(
     2
   )} €`;
   document.getElementById("ticket-number").innerText = ticketNumber;
-  document.getElementById("ticket-wagon").innerText = ticketWagon;
+  document.getElementById("ticket-carriage").innerText = ticketCarriage;
   document.getElementById("ticket-section").classList.add("active");
 }
+
+// Imposto l'evento per ricaricare al click del bottone annulla
+const reloadButton = document.getElementById("reload-page");
+reloadButton.addEventListener("click", function () {
+  window.location.reload();
+});
